b262a12a7767f9ed30dd7a2904ef9a6a557a56ce
tommaso.zammarchi <tommaso.zammarchi@PC7324>
2020-11-18 11:12:00 +0100

    Aggiunta Metodo Ricerca e aggiunta del libro

---
+++ GestioneBiblioteca/GestioneBiblioteca/ClasseBiblioteca.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GestioneBiblioteca
+{
+    public class ClasseBiblioteca
+    {
+        private int _nome;
+        private int _indirizzi;
+    }
+}
